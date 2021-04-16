2b7e06daf049eb6eba85702fea86e097c6e02c05
williamnrivero <williamnrivero@gmail.com>
2021-04-16 15:07:56 -0600

    Updated HTML file and created time blocks

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// var timeBlocks = document.queryselector(".container")
+
+$(document).ready(function(){
+
+    $('.time-block').each(function() {
+    
+
+    })
+})
+
+
+// Moment.js format
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do"));
